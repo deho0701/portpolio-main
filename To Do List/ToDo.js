d361a916b216e984e469e4d50e1d7cdb39ff6cea
deho0701 <54195905+deho0701@users.noreply.github.com>
2020-11-26 12:58:45 +0900

    체크버튼 추가/9개이상 못넘어가게 제한/입력창 디자인/추가로 체크했을떄 흐려지게 해야함

--- To Do List/ToDo.js
+++ To Do List/ToDo.js
@@ -1,6 +1,8 @@
 var button = document.getElementById('input_button');
 var input = document.getElementById('input_text');
 var list = document.getElementById('list');
+var checkbox =document.getElementById('checkbox');
+var title= document.getElementById('title');
 
 if(button){
     button.addEventListener("click", clickButton);
@@ -8,19 +10,38 @@ if(button){
     console.log("다시 시도해주세요");
 }
 
-
+function max_line(count){
+  if(count==9){
+    // window.onload.title.innerHTML="9 is max";
+    
+    return 0;
+  }
+  else{
+    return 1;
+  }
+}
 var cnt = 1;
 
 function clickButton() {
+  if(max_line(list.childElementCount)==0){
+    alert("오늘은 이정도면 충분해요 :)");
+    
+    return 0;
+  }
   var temp = document.createElement('li');
   temp.setAttribute("class", "list-group-item");
   temp.setAttribute("id", "li"+cnt);
   temp.innerHTML = input.value;
-  temp.innerHTML += "<button style='float: right;' class='btn-outline-secondary' type='button' onclick='remove("+cnt+")'>delete</button>";
+  temp.innerHTML += "<input type='checkbox' id='checkbox' onclick='clicked()'><button style='float: right;' class='btn-outline-secondary' type='button' onclick='remove("+cnt+")'>delete</button>";
   list.appendChild(temp);
   cnt++;
 }
 
+// function clicked(){
+//   if(true){
+//     Element.('color','red');
+//   }
+// }
 
 function remove(cnt) {
     var li = document.getElementById('li'+cnt);
